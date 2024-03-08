b31dc5d24b2c76b563438fe1439d52b7b3022155
Gerry-Latris <whateveritakes10@gmail.com>
2024-03-08 20:58:58 +0800

    Fix Tailwind and Material Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@material-tailwind/react";
 function App() {
   return (
     <div>
-      <h1>Yabali Homepage</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
     </div>
   );
 }
