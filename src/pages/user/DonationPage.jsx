dd21d393d72f421f3fc5f639fb539e4cc07c3814
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-03-09 15:00:08 +0800

    Add Why They Need to Help Section

--- src/pages/user/DonationPage.jsx
+++ src/pages/user/DonationPage.jsx
@@ -8,7 +8,7 @@ const DonationPage = () => {
       <section className="Donate">
         <div className="flex gap-12 p-16">
           <div className="flex w-1/2">
-            <LineSpan></LineSpan>
+            <LineSpan margin="mr-8"></LineSpan>
             <div className="flex flex-col gap-7">
               <h5>Donasi</h5>
               <BigTitle text="Mulai Memberi Sekarang" />
@@ -34,10 +34,107 @@ const DonationPage = () => {
           </div>
         </div>
       </section>
+      <section className="Why They Need to Help ?">
+        <div className="flex flex-col gap-16 p-16">
+          <div className="flex gap-12">
+            <div className="w-3/5">
+              <img src="/public/donation/why-need-support.png" alt="" />
+            </div>
+            <div className="flex w-2/5">
+              <div className="flex flex-col items-end w-full gap-5 text-end">
+                <h5>Mengapa Harus Mulai Peduli</h5>
+                <BigTitle text="Kenapa Mereka Butuh Dukungan Kita?"></BigTitle>
+              </div>
+              <LineSpan margin="ml-8"></LineSpan>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-8">
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">Masalah Psikologis</h3>
+              </div>
+              <p>
+                Banyak anak mengalami trauma atau masalah psikologis akibat
+                kondisi lingkungan atau kejadian yang mereka alami. Dukungan
+                terapeutik dan psikososial sangat penting untuk membantu mereka
+                pulih dan bangkit kembali.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">
+                  Kekerasan, Eksploitasi, dan Pengabaian
+                </h3>
+              </div>
+              <p>
+                Setiap anak berhak atas lingkungan yang aman dan mendukung. Kami
+                berjuang untuk melindungi anak-anak dari kekerasan, eksploitasi,
+                dan pengabaian, namun upaya ini memerlukan sumber daya yang
+                tidak sedikit.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">
+                  Kurang Perhatian dan Dukungan
+                </h3>
+              </div>
+              <p>
+                Banyak anak yang kami dukung berasal dari keluarga kurang mampu,
+                daerah terpencil, atau lingkungan yang tidak mendukung
+                pertumbuhan dan pengembangan mereka secara optimal.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">
+                  Masalah Gizi dan Biaya Perobatan
+                </h3>
+              </div>
+              <p>
+                Untuk tumbuh kembang yang optimal, setiap anak memerlukan asupan
+                gizi dan akses ke layanan kesehatan yang memadai. Sayangnya, hal
+                ini masih menjadi barang langka dengan biaya yang besar bagi
+                sebagian besar anak yang kami layani.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">
+                  Kurangnya Akses Pendidikan
+                </h3>
+              </div>
+              <p>
+                Pendidikan adalah senjata paling ampuh yang bisa digunakan untuk
+                mengubah dunia. Namun, tidak semua anak mendapatkan kesempatan
+                untuk bersekolah, apalagi menerima pendidikan berkualitas.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <i></i>
+                <h3 className="text-lg font-bold">
+                  Kurangnya Akses Pendidikan
+                </h3>
+              </div>
+              <p>
+                Pendidikan adalah senjata paling ampuh yang bisa digunakan untuk
+                mengubah dunia. Namun, tidak semua anak mendapatkan kesempatan
+                untuk bersekolah, apalagi menerima pendidikan berkualitas.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="Where Your Donation Go">
         <div className="flex gap-12 p-16 bg-orange-100">
           <div className="flex w-1/2">
-            <LineSpan></LineSpan>
+            <LineSpan margin="mr-8"></LineSpan>
             <div className="flex flex-col gap-7">
               <h5>Bagaimana Donasi Digunakan</h5>
               <BigTitle text="Bagaimana kami menggunakan donasi Anda" />
@@ -94,6 +191,27 @@ const DonationPage = () => {
                 kemitraan dengan pihak berwenang untuk menciptakan lingkungan
                 yang lebih aman bagi anak-anak.
               </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="Donatur dan Transparansi">
+        <div className="p-16">
+          <div className="flex flex-col items-center gap-16">
+            <div className="flex flex-col gap-5 text-center max-w-[600px]">
+              <BigTitle text="Donatur dan Transparansi"></BigTitle>
+              <p>
+                Transparansi kepada donatur adalah prioritas kami. Kami
+                berkomitmen untuk menyediakan laporan berkala yang menunjukkan
+                bagaimana donasi Anda telah digunakan untuk membuat dampak
+                positif. Kami juga menjaga setiap Transparansi para donatur yang
+                tidak ingin memberikan tanda informasi pribadi.
+              </p>
+            </div>
+            <div>
+              <table>
+                <p>Disini table</p>
+              </table>
             </div>
           </div>
         </div>
